31c8b2508c00ecf443b85503a39d63041fa1d2ce
Carson Potter <cp3192@gmail.com>
2021-02-14 13:40:41 -0700

    shipping form progress, data handling

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 
 
@@ -7,15 +7,13 @@ import Shipping from "./components/Shipping";
 import { HomePage } from "./index";
 import history from './history';
 
-export default class Routes extends Component {
-    render() {
+export default function Routes(props) {
         return (
             <Router history={history}>
                 <Switch>
-                    <Route path="/" exact component={HomePage} />
+                    <Route path="/" exact component={(props) => <HomePage /> }/>
                     <Route path="/shipping" component={Shipping} />
                 </Switch>
             </Router>
         )
-    }
 }
--- src/components/Shipping.jsx
+++ src/components/Shipping.jsx
@@ -2,35 +2,45 @@ import React, { Component } from 'react';
 import ReactDOM, { Render } from 'react-dom';
 import '../index.css'; // Maybe add shipping CSS
 // import App from './App';
+// import { useLocation } from "react-router-dom";
+import history from '../history';
+import { useFormik } from "formik";
+
 
 export default function Shipping(props) {
+	console.log(props.location)
+	const selectedHotdogs = props.location.state.selectedHotdogs
+	console.log(JSON.stringify(selectedHotdogs))
+	const amt = selectedHotdogs.length * 12;
+	const cardholderFields = [('firstName', 'Haute'),
+						 ('lastName', 'Doug'),
+                         ('addr1', '1 Bratwurst St'),
+                         ('city', 'Frankfurt'),
+                         ('state', 'CA'),
+                         ('zip', '94101'),
+                         ('email', 'holdthemustard@hotdog.com'),
+                         ('phone', '4645553633')];
+    const cardholderData = []
+
 	return (
-		<div className="columns">
-        <div className="hero column col-auto col-mx-auto">
-          <div className="panel">
+		<div className="columns" id="shipping">
             <div className="panel-header">
               <div className="panel-title">
-                <h6>Shipping &amp; Billing Info</h6>
+                <h4>Your <span class="text-error">🌭</span> Pins</h4>
+                <h6>Subtotal: &#36;{amt}.00</h6>
+              </div>
+			<div id="shippingImg">
+				<HotdogImages dogs = {selectedHotdogs}/>
+			</div>
+          </div>
+        <div className="hero column col-auto col-mx-auto">
+          <div className="panel" id="shippingInfoForm">
+            <div className="panel-header">
+              <div className="panel-title">
+                <h4>Shipping &amp; Billing Info</h4>
               </div>
             </div>
-            <form className="form-horizontal" action="/buy" method="post">
-              <input name="items" type="hidden" defaultValue="{{ items }}" />
-              <div className="panel-body">
-                <div className="form-group">
-                  <div className="col-3 col-sm-12">
-                    <label className="form-label" htmlFor="cardholder-{{ field[0]}}">TEST</label>
-                  </div>
-                  <div className="col-9 col-sm-12">
-                    <input className="form-input" type="text" name="{{ field[0] }}" id="cardholder-{{ field[0] }}" placeholder="{{ field[1] }}" />
-                  </div>
-                </div>
-              </div>
-              <div className="panel-footer">
-                <button className="btn btn-primary" id="submit">
-                  Next →
-                </button>
-              </div>
-            </form>
+            	<ShippingInfoForm items= {selectedHotdogs} />
           </div>
         </div>
       </div>
@@ -38,3 +48,102 @@ export default function Shipping(props) {
 
 	)
 }
+
+
+const HotdogImages = props => {
+	const imgs = props.dogs.map(({ hotdog, availableHotdogsIndex }, i) => {
+		return (
+			<img
+				style={{ margin: "auto", maxWidth: 150 }}
+	            src={process.env.PUBLIC_URL + '../previews/' + hotdog + ".png"}
+	        />
+		);
+	});
+	return imgs;
+}
+
+const ShippingInfoForm = props => {
+	const formik = useFormik({
+		initialValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			addr1: '',
+			state: '',
+			zip: '',
+			phone: ''
+		},
+		onSubmit: values => {
+			//DO SOMETHING HERE... Likely push to history and update path.
+		}
+	});
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<label htmlFor="firstName"> First Name</label>
+			<input
+				id="firstName"
+		        name="firstName"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.firstName}
+       		/>
+
+			<label htmlFor="lastName"> Last Name</label>
+			<input
+				id="lastName"
+		        name="lastName"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.lastName}
+       		/>
+
+			<label htmlFor="addr1"> Address</label>
+			<input
+				id="addr1"
+		        name="addr1"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.addr1}
+       		/>
+
+			<label htmlFor="state"> State</label>
+			<input
+				id="state"
+		        name="state"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.state}
+       		/>
+
+			<label htmlFor="zip"> Zip</label>
+			<input
+				id="zip"
+		        name="zip"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.zip}
+       		/>
+
+			<label htmlFor="email"> email</label>
+			<input
+				id="email"
+		        name="email"
+		        type="email"
+		        onChange={formik.handleChange}
+		        value={formik.values.email}
+       		/>
+
+			<label htmlFor="phone"> phone</label>
+			<input
+				id="phone"
+		        name="phone"
+		        type="text"
+		        onChange={formik.handleChange}
+		        value={formik.values.firstName}
+       		/>
+
+       		<button type="submit" id="submit"> Next → </button>
+       		
+       	</form>
+	);
+};
